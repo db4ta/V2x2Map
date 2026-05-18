dffc8d829f7c99ffa59b2b1a14c80bca77d9d22a
Alexander Brosi <alexanderbrosi@gmx.de>
2026-05-18 18:19:02 +0200

    Initial Commit

---
+++ v2x2map/v2x2mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  v2x2mapApp.swift
+//  v2x2map
+//
+//  Created by Alex on 18.05.26.
+//
+
+import SwiftUI
+
+@main
+struct v2x2mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
